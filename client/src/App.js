e47bfaa83bd020da9fd4563dfc184bc1b4022d06
Legionnaire <blake.marter@gmail.com>
2019-07-17 20:02:11 -0700

    adding blank pages ready for edit

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Books from "./pages/Books";
-import Detail from "./pages/Detail";
+import Main from "./pages/Main";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
 
@@ -13,8 +13,8 @@ function App() {
         <Switch>
           <Route exact path="/" component={Books} />
           <Route exact path="/books" component={Books} />
-          <Route exact path="/books/:id" component={Detail} />
-          <Route exact path="/auth/google" />
+          <Route exact path="/books/:id" component={Main} />
+
           <Route component={NoMatch} />
         </Switch>
       </div>
