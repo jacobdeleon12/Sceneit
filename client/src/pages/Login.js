122c569dd556863122040e2e700fc2b0f5f2be3d
kbub27 <kalebmerriman27@gmail.com>
2019-07-19 20:28:44 -0700

    fixed login function

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -3,20 +3,24 @@ import { Col, Row, Container } from "../components/Grid";
 import { GLogin } from "../components/Buttons/Google/index";
 import NavBar from "../components/Nav/index";
 
-export default function Login() {
+class Login extends Component {
 
-    return (
-        <div>
-            <NavBar />
-            <Container fluid>
-                <Row>
-                    <Col size="sm-12">
-                        <div className="m-3 p-3 text-center">
-                            <GLogin />
-                        </div>
-                    </Col>
-                </Row>
-            </Container>
-        </div>
-    );
+    render() {
+        return (
+            <div>
+                <NavBar />
+                <Container fluid>
+                    <Row>
+                        <Col size="sm-12">
+                            <div className="m-3 p-3 text-center">
+                                <GLogin />
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        );
+    };
 };
+
+export default Login;
