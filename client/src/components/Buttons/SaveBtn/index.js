09bc861bd3dcf1654c3495e00aa8c273f8d1c30f
kbub27 <kalebmerriman27@gmail.com>
2019-07-20 12:37:24 -0700

    added a save button for all the videos

---
+++ client/src/components/Buttons/SaveBtn/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+// The ...props means, spread all of the passed props onto this element
+// That way we don't have to define them all individually
+function SaveBtn(props) {
+  return (
+    <button className="save-btn pb-2" {...props} role="button" tabIndex="0">
+      Save &#10004;
+    </button>
+  );
+}
+
+export default SaveBtn;
