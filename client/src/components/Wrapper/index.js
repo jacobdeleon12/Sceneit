85fcaf369748231b1dffb319c188a608aed7cf47
Kai R <kairichardson2000@gmail.com>
2019-07-21 22:12:05 -0700

    side scrolling

--- client/src/components/Wrapper/index.js
+++ client/src/components/Wrapper/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./style.css";
 
 function Wrapper(props) {
-  return <div className="wrapper container justify-content-center">{props.children}</div>;
+  return <div className="scrollmenu wrapper container">{props.children}</div>;
 }
 
 export default Wrapper;
