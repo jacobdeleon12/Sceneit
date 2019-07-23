ce7e698519aa1ed70566c9adbd03c251147a85a9
Jacob DeLeon <jacobdeleon12@gmail.com>
2019-07-22 18:26:08 -0700

    added google font

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -4,14 +4,15 @@
 // export * from "./SearchResultNav";
 // export * from "./TitleNav";
 // export * from "./UserNav";
+import "./style.css"
 
 import React from "react";
-
+const name = "<SceneIT/>"
 function NavBar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
       <a className="navbar-brand" href="/">
-        SceneIT
+        {name}
       </a>
     </nav>
   );
