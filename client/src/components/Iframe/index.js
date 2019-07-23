79804fa00bcb18db30ca3926ff617fbcf4627369
Kai R <kairichardson2000@gmail.com>
2019-07-22 19:17:25 -0700

    line organization

--- client/src/components/Iframe/index.js
+++ client/src/components/Iframe/index.js
@@ -4,15 +4,28 @@ import "./style.css";
 export function JumboIframe(props) {
   return (
     <div className="pb-0 card text-center">
-      <iframe title={props.name} style={{height: "100%"}} src={"https://www.youtube.com/embed/" + props.YTstr } frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+      <iframe
+        title={props.name}
+        style={{ height: "100%" }}
+        src={"https://www.youtube.com/embed/" + props.YTstr}
+        frameBorder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
     </div>
   );
 }
 
 export function Iframe(props) {
   return (
-    <div className="iFrame pb-0 text-center" >
-      <iframe title={props.name} src={"https://www.youtube.com/embed/" + props.YTstr} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+    <div className="iFrame pb-0 text-center">
+      <iframe
+        title={props.name}
+        src={"https://www.youtube.com/embed/" + props.YTstr}
+        frameBorder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
     </div>
   );
 }
