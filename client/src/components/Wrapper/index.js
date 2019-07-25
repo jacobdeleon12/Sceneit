8fe34e4217ef62ca28f265995b28876ac41dc63a
Kai Richardson <kairichardson2000Gmail.com>
2019-07-24 19:06:29 -0700

    got buttons in place

--- client/src/components/Wrapper/index.js
+++ client/src/components/Wrapper/index.js
@@ -3,9 +3,17 @@ import "./style.css";
 
 function Wrapper(props) {
   return (
-    <nav class=".wrapper">
-      <div className="scroll_menu">{props.children}</div>
-    </nav>
+    <div className=".wrapper">
+      <div className="scroll_menu">
+        <div id="slideLeft" className="scrollBtn">
+          &lt;
+        </div>
+        {props.children}
+        <div id="slideRight" className="scrollBtn">
+          &gt;
+        </div>
+      </div>
+    </div>
   );
 }
 
