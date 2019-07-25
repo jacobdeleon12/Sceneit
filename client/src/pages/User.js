f3f29270cdcca34012952487ca948fea72fc66c0
kbub27 <kalebmerriman27@gmail.com>
2019-07-24 20:47:40 -0700

    iframe style

--- client/src/pages/User.js
+++ client/src/pages/User.js
@@ -3,7 +3,6 @@ import React, { Component } from "react";
 import { Col, Row, Container } from "../components/Grid";
 import Jumbotron from "../components/Jumbotron";
 import API from "../utils/API";
-import Wrapper from "../components/Wrapper";
 import Userwrap from "../components/Userwrap";
 import NavBar from "../components/Nav/MainNav";
 import { Iframe } from "../components/Iframe";
