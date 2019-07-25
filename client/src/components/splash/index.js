de3eaa6f4b58f942d638c97f1b975950c6fa84a8
Jacob DeLeon <jacobdeleon12@gmail.com>
2019-07-24 19:58:24 -0700

    updating splash

--- client/src/components/splash/index.js
+++ client/src/components/splash/index.js
@@ -2,6 +2,8 @@ import "./style.css";
 import React from "react";
 import { Col, Row, Container } from "../Grid/index";
 import Img from "./img/down-arrow2.png";
+import { GLogin } from "../Buttons/Google/index";
+
 
 function background({ children }) {
   return (
@@ -22,6 +24,7 @@ function background({ children }) {
               <img className="splashImg ball" alt="arrow" src={Img} />
               <br />
               <br />
+              <GLogin/>
             </div>
           </Col>
         </Row>
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
-// import { Col, Row, Container } from "../components/Grid";
 import NavBar from "../components/Nav/index";
 import Background from "../components/splash";
-import { GLogin } from "../components/Buttons/Google";
+
 
 class Login extends Component {
   render() {
