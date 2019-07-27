07f796800031116c1ba17a9759b280e04288555a
Kai Richardson <kairichardson2000Gmail.com>
2019-07-27 14:16:14 -0700

    getting cors for steam

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -2,11 +2,23 @@ import React, { Component } from "react";
 import NavBar from "../components/Nav/index";
 import Background from "../components/splash";
 
-import addToDb from "../video";
+import func from "../video";
+
+// function addEntry() {
+//   // Parse any JSON previously stored in allEntries
+//   var existingEntries = JSON.parse(localStorage.getItem("allEntries"));
+//   if(existingEntries == null) existingEntries = [];
+//   localStorage.setItem("entry", JSON.stringify(addToDb));
+
+//   // Save allEntries back to local storage
+//   existingEntries.push(entry);
+//   localStorage.setItem("allEntries", JSON.stringify(existingEntries));
+// };
 
 class Login extends Component {
   render() {
-    addToDb();
+    func();
+
     return (
       <div>
         <NavBar />
--- client/src/video/index.js
+++ client/src/video/index.js
@@ -2,9 +2,9 @@ import TMDB from "../video/tmdb";
 // import STEAM from "../video/steam";
 // import REDDIT from "../video/reddit";
 // import YOUTUBE from "../video/youtube";
-import API from "../utils/API";
+// import API from "../utils/API";
 
-const tmbdObjId = "5d3cb2a83bef5a333c9ff6c0";
+// const tmbdObjId = "5d3cb2a83bef5a333c9ff6c0";
 // const steamObjId = "5d3cb2a83bef5a333c9ff6c1";
 // const youtubeObjId = "5d3cb2a83bef5a333c9ff6c2";
 // const redditObjId = "5d3cb2a83bef5a333c9ff6c3";
@@ -20,33 +20,38 @@ const tmbdObjId = "5d3cb2a83bef5a333c9ff6c0";
 // YOUTUBE.searchName("speed");
 
 async function addToDb() {
+  // const bigObj = [];
   try {
     const TMDBarr = await TMDB.searchList("popularity");
     // const STEAMarr = await STEAM.searchList("popularwishlist");
     // const REDDITarr = await REDDIT.searchList("videos");
     // const YOUTUBEarr = await YOUTUBE.searchList("mostPopular");
 
+    // console.log(TMDBarr);
+    // console.log(STEAMarr);
+    // console.log(REDDITarr);
+    // console.log(YOUTUBEarr);
+
+    // await bigObj.push(TMDBarr, STEAMarr, REDDITarr, YOUTUBEarr);
+    return TMDBarr;
+
     // const TMDBarr = await TMDB.searchName("speed");
     // const STEAMarr = await STEAM.searchName("speed");
     // const REDDITarr = await REDDIT.searchName("speed");
     // const YOUTUBEarr = await YOUTUBE.searchName("speed");
 
-    API.saveVideoObj(tmbdObjId, {
-      $set: {
-        vidType: "tmdb",
-        videos: TMDBarr
-      }
-    })
-      .then(res => {
-        console.log("New video info added to DB");
-        // console.log(res);
-      })
-      .catch(err => console.log(err));
+    // API.saveVideoObj(tmbdObjId, {
+    //   $set: {
+    //     vidType: "tmdb",
+    //     videos: TMDBarr
+    //   }
+    // })
+    //   .then(res => {
+    //     console.log("New video info added to DB");
+    //     // console.log(res);
+    //   })
+    //   .catch(err => console.log(err));
 
-    // console.log(TMDBarr);
-    // console.log(STEAMarr);
-    // console.log(REDDITarr);
-    // console.log(YOUTUBEarr);
   } catch (error) {
     console.log(error.message);
   }
