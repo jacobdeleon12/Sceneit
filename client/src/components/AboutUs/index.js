d93abdb2d4bd8bf687c4ccfd969d16a26eb364f8
Jacob DeLeon <jacobdeleon12@gmail.com>
2019-08-03 12:31:26 -0700

    about us style

--- client/src/components/AboutUs/index.js
+++ client/src/components/AboutUs/index.js
@@ -8,11 +8,121 @@ function AboutUS() {
   return (
     <div className="About">
       <Container className="fluid">
+        <h1 className="text-center aboutHeader">The Dev Team</h1>
         <Row>
           <Col size="12">
-          <img src = "https://via.placeholder.com/150" alt="placeholder"></img>
-          <p>here is some text</p>
+            <div className="text-left row AboutEach">
+              <img
+                className="col-12 col-sm-4 headShots mt-2"
+                src="https://via.placeholder.com/150"
+                alt="placeholder"
+                style={{ height: "250px" }}
+              ></img>
 
+              <p className="col Description">
+                <h3 className="headerName">Jacob DeLeon</h3>
+                Full Stack Web Developer with a Background in design and
+                animation. Attended Both Washington State University and
+                University of Washington and to obtain a bachelor’s in Digital
+                Technology and Full Stack Web Development certificate.
+                Passionate about technology and creativity which led to a path
+                of schooling, work experience, and teamwork which strengthened
+                communication skills, and the ability to connect with all
+                individuals. This combination enabled proficiencies in: Html,
+                CSS, JavaScript, jQuery, Node, React. As a result, I built many
+                successful projects. Currently, pursuing a position as a Full
+                Stack Web Developer molded and sculpted from education and
+                experience to be one of the very best developers a technological
+                company would desire.
+              </p>
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col size="12">
+            <div className="text-left row AboutEach">
+              <img
+                className="col-12 col-sm-4 headShots mt-2"
+                src="https://via.placeholder.com/150"
+                alt="placeholder"
+                style={{ height: "250px" }}
+              ></img>
+
+              <p className="col Description">
+                <h3 className="headerName">Jacob DeLeon</h3>
+                Full Stack Web Developer with a Background in design and
+                animation. Attended Both Washington State University and
+                University of Washington and to obtain a bachelor’s in Digital
+                Technology and Full Stack Web Development certificate.
+                Passionate about technology and creativity which led to a path
+                of schooling, work experience, and teamwork which strengthened
+                communication skills, and the ability to connect with all
+                individuals. This combination enabled proficiencies in: Html,
+                CSS, JavaScript, jQuery, Node, React. As a result, I built many
+                successful projects. Currently, pursuing a position as a Full
+                Stack Web Developer molded and sculpted from education and
+                experience to be one of the very best developers a technological
+                company would desire.
+              </p>
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col size="12">
+            <div className="text-left row AboutEach">
+              <img
+                className="col-12 col-sm-4 headShots mt-2"
+                src="https://via.placeholder.com/150"
+                alt="placeholder"
+                style={{ height: "250px" }}
+              ></img>
+
+              <p className="col Description">
+                <h3 className="headerName">Jacob DeLeon</h3>
+                Full Stack Web Developer with a Background in design and
+                animation. Attended Both Washington State University and
+                University of Washington and to obtain a bachelor’s in Digital
+                Technology and Full Stack Web Development certificate.
+                Passionate about technology and creativity which led to a path
+                of schooling, work experience, and teamwork which strengthened
+                communication skills, and the ability to connect with all
+                individuals. This combination enabled proficiencies in: Html,
+                CSS, JavaScript, jQuery, Node, React. As a result, I built many
+                successful projects. Currently, pursuing a position as a Full
+                Stack Web Developer molded and sculpted from education and
+                experience to be one of the very best developers a technological
+                company would desire.
+              </p>
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col size="12">
+            <div className="text-left row AboutEach">
+              <img
+                className="col-12 col-sm-4 headShots mt-2"
+                src="https://via.placeholder.com/150"
+                alt="placeholder"
+                style={{ height: "250px" }}
+              ></img>
+
+              <p className="col Description">
+                <h3 className="headerName">Jacob DeLeon</h3>
+                Full Stack Web Developer with a Background in design and
+                animation. Attended Both Washington State University and
+                University of Washington and to obtain a bachelor’s in Digital
+                Technology and Full Stack Web Development certificate.
+                Passionate about technology and creativity which led to a path
+                of schooling, work experience, and teamwork which strengthened
+                communication skills, and the ability to connect with all
+                individuals. This combination enabled proficiencies in: Html,
+                CSS, JavaScript, jQuery, Node, React. As a result, I built many
+                successful projects. Currently, pursuing a position as a Full
+                Stack Web Developer molded and sculpted from education and
+                experience to be one of the very best developers a technological
+                company would desire.
+              </p>
+            </div>
           </Col>
         </Row>
       </Container>
