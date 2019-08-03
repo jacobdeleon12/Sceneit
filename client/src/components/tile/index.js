93f09199c08752f5fbd73dd4c8942cf5a82b1d5b
Legionnaire <blake.marter@gmail.com>
2019-08-03 00:13:28 -0700

    fixed paths and file names, and took out some console logs

--- client/src/components/tile/index.js
+++ client/src/components/tile/index.js
@@ -7,7 +7,7 @@ export function Tile(props) {
 export function JumboTile({ children }) {
   return (
     <div
-      style={{ clear: "both", paddingTop: "2rem", paddingBottom: "2rem", textAlign: "center",  marginTop: "1rem", }}
+      style={{ clear: "both", paddingTop: "2rem", paddingBottom: "2rem", textAlign: "center", marginTop: "1rem", }}
       className="jumbotron justify-content-center text-center"
     >
       {children}
