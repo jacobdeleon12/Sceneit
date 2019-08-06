76e470b8f99bcb1dc96542f51b49b8e07238333f
jacobdeleon12 <jacobdeleon12@gmail.com>
2019-08-06 16:57:09 -0700

    css for mobile

--- client/src/components/Tile/index.js
+++ client/src/components/Tile/index.js
@@ -4,7 +4,7 @@ export function Tile(props) {
   return (
     <div
       className="tile"
-      style={{ paddingTop: "1.2rem", paddingBottom: "13rem" }}
+      // style={{ paddingTop: "1.2rem", paddingBottom: "13rem" }}
     >
       {props.children}
     </div>
