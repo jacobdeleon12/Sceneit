bab46f54a6d97f04e419c5d0c05b18a07904144e
kbub27 <kalebmerriman27@gmail.com>
2019-08-06 21:25:29 -0700

    console errors taken care of

--- client/src/components/SearchWrapperVideos/index.js
+++ client/src/components/SearchWrapperVideos/index.js
@@ -132,7 +132,7 @@ export default class mainWrapper extends React.Component {
         <h1>Searching for: {pathSnip}</h1>
         <br />
         <h5 className="load text-center">
-          Loading <i class="fas fa-spinner fa-spin"></i>
+          Loading <i className="fas fa-spinner fa-spin"></i>
         </h5>
       </div>
     ) : (
