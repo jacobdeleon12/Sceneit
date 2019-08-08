be38c1898faf94dc6a89803437c23e159316b92e
Kai Richardson <kairichardson2000Gmail.com>
2019-08-07 18:25:24 -0700

    Update tmdb.js

--- video/tmdb.js
+++ video/tmdb.js
@@ -1,9 +1,6 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
-
 TMDB_API = "f156e9ea";
-
-
 module.exports = {
   // try "trailers"
   // Queries TMDB list, returns 20 videos
@@ -14,7 +11,6 @@ module.exports = {
         .get(`https://www.imdb.com/movies-coming-soon`)
         .then(response => {
           let $ = cheerio.load(response.data);
-
           $("td.overview-top").each(function(i, element) {
             const name = $(element)
               .find("h4")
@@ -24,18 +20,16 @@ module.exports = {
               .find("h4")
               .find("a")
               .attr("href");
-
             axios.get(`https://www.imdb.com${imdbId}`).then(response => {
               let $ = cheerio.load(response.data);
               const trailerID = $("a.slate_button").attr("data-video");
               const poster = $("a.slate_button")
                 .find("img")
                 .attr("src");
-
               if (i >= 20) {
                 return false;
               } else {
-                poster &&
+                trailerID &&
                   urlArray.push({
                     type: "imdb",
                     name: name,
@@ -52,7 +46,6 @@ module.exports = {
         .catch(err => reject(err));
     });
   },
-
   // try "speed"
   // Queries TMDB name, returns 10 videos
   searchName: function(query) {
@@ -63,13 +56,11 @@ module.exports = {
         .then(response => {
           let movieArr = response.data.Search;
           let count = 0;
-          // console.log(response.data.Search);
-
+          // console.log(movieArr);
           if (movieArr) {
             for (const movie of movieArr) {
               const name = movie.Title;
               const imdbId = movie.imdbID;
-
               axios
                 .get(`https://www.imdb.com/title/${imdbId}`)
                 .then(response => {
@@ -78,12 +69,10 @@ module.exports = {
                   const poster = $("a.slate_button")
                     .find("img")
                     .attr("src");
-
                   count++;
                   console.log(count);
                   // console.log(trailerID);
-
-                  poster &&
+                  trailerID &&
                     urlArray.push({
                       type: "imbd",
                       name: name,
@@ -91,8 +80,10 @@ module.exports = {
                       bigImg: poster,
                       url: `https://www.imdb.com/videoembed/${trailerID}`
                     });
-                  console.log(urlArray);
-                  resolve(urlArray);
+                  if (count >= 9) {
+                    // console.log(urlArray);
+                    resolve(urlArray);
+                  }
                 })
                 .catch(err => reject(err));
             }
